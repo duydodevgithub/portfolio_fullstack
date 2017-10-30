9c78cd86e1e1b858d5cc804d45fad01fe502ac68
DuyDo <duydo.dev@gmail.com>
2017-10-30 02:18:11 -0500

    change greeting sentence in main.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,11 +3,11 @@
 var d = new Date();
 d = d.getHours();
 if(d > 18) {
-    $("#sayHello").html("Good Evening! Welcome to my page");
+    $("#sayHello").html("Good Evening!");
 } else if (d > 12) {
-    $("#sayHello").html("Good Afternoon! Welcome to my page");
+    $("#sayHello").html("Good Afternoon!");
 } else if (d > 0) {
-    $("#sayHello").html("Good Morning! Welcome to my page");
+    $("#sayHello").html("Good Morning!");
 } else {
     $("#sayHello").html("Welcome to my page!");    
 }
