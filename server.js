5b99927247dd93586a25f76b18d90a249e757127
DuyDo <duydo.dev@gmail.com>
2018-04-13 14:55:18 -0500

    remove handles bar

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 // dependencies
 const express = require("express");
 const app = express();
-const request = require("request");
 const path = require("path");
-const exphbs = require("express-handlebars");
 
 //define PORT
 const PORT = process.env.PORT || 3002;
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-//let express to serve static files
-app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/", function(res, res){
     res.render("index");
